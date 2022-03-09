6f9776bba85cf9098e526fde51ac72ed86a21017
ILDeepak <90624320+ILDeepak@users.noreply.github.com>
2022-03-09 17:56:16 +0530

    added result in kyc status

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/application_journey/KycStatusApiResponse.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/application_journey/KycStatusApiResponse.kt
@@ -4,9 +4,16 @@ import com.mvalu.bettr_api.network.ApiBaseResponse
 import com.squareup.moshi.Json
 
 class KycStatusApiResponse : ApiBaseResponse() {
+
+    @field:Json(name = "results")
+    var results: KycStatusResult? = null
+
+class KycStatusResult {
+    
     @field:Json(name = "status")
     var status: Boolean? = null
 
     @field:Json(name = "kycStatus")
     var kycStatus: String? = null
+    }
 }
