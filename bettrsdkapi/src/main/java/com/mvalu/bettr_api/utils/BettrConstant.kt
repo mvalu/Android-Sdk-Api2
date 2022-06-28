c6e3c677c4801fd7d407be405e24bb78ebf2b46d
Indialends <mohdashfaq@indialends.com>
2022-06-28 16:57:45 +0530

    no message

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
@@ -1,8 +1,8 @@
 package com.mvalu.bettr_api.utils
 
 //dev
-//const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
-const val BASE_URL = "https://prepaidapidev.bettrcredit.com/api/"
+const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
+
 
 //pre-prod
 //const val BASE_URL = "https://api-new.mvalu.ml/api/"
